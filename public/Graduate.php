f111090a09e1e45047212943147e7cea9807ffd5
uta-mxh1329 <85514173+uta-mxh1329@users.noreply.github.com>
2022-03-17 14:07:13 -0500

    updated Graduate.php
    section for former graduate and current graduate

--- public/Graduate.php
+++ public/Graduate.php
@@ -73,44 +73,73 @@ html {
 </style>
 
 <div class="jumbotron" style="background-color: white";>
-             <h1 style="padding: 20px; border-width: 5px; border-style: solid;">Graduate Students</h1>
-         </div> <br><br>
+    <h1 style="padding: 20px; border-width: 5px; border-style: solid;">Graduate Students</h1>
+</div> 
+
+<br><br>
 
 <?php include "SimpleXLSX.php";?>
 
+<!-- Information about Graduate students -->
 <?php 
-if ( $xlsx = SimpleXLSX::parse('graduates.xlsx') ) {
-// echo '<table><tbody>';
-$i = 0;
-  foreach ($xlsx->rows() as $elt){
-// for ($i=0; $i<=4; $i++) {
-  if ($i == 0) {
-    echo '<div class="row">';
-    // echo "<tr><th>" . $elt[0] . "</th><th>" . $elt[1] . "</th></tr>";
-  } else {
-    // echo "<tr><td>" . $elt[0] . "</td><td>" . $elt[1] . "</td><td>". $elt[2] . "</td><tr>" ;
-    echo '<div class="column">
-    <div class="card">
-    <img class="img-pro" src="'.$elt[7].'" alt="Jane">
-      <div class="container">
-        <h2>'.$elt[1].'</h2>
-        <p class="title">'.$elt[2].'</p>
-        <p class="title">'.$elt[3].'</p>
-        <p><a href='.$elt[5].'>LinkedIn</a></p>
-        <p><a href="mailto:'.$elt[6].'" <button class="button">Contact</button></a></p>
-      </div>
-    </div>
-  </div>';
-  }   
-$i = $i + 1;   
-}
-echo '</div>';
-// echo "</tbody></table>";
-
+if ( $xlsx = SimpleXLSX::parse('data/current_graduates.xlsx') ) {
+  $i = 0;
+    foreach ($xlsx->rows() as $elt){
+      if ($i == 0) {
+        echo '<div class="row">';
+      } else {
+        echo '<div class="column">
+        <div class="card">
+        <img class="img-pro" src="'.$elt[7].'" alt="Jane">
+          <div class="container">
+              <h2>'.$elt[1].'</h2>
+              <p class="title">'.$elt[2].'</p>
+              <p class="title">'.$elt[3].'</p>
+              <p><a href='.$elt[5].'>LinkedIn</a></p>
+              <p><a href="mailto:'.$elt[6].'" <button class="button">Contact</button></a></p>
+            </div>
+          </div>
+        </div>';
+      }   
+  $i = $i + 1;   
+  }
+  echo '</div>';
 } else {
   echo SimpleXLSX::parseError();
 }
+?>
 
+<!-- information about former graduate students -->
+<br><br>
+<div class="jumbotron" style="background-color: white";>
+    <h1 style="padding: 20px; border-width: 5px; border-style: solid;">Former Graduate Students</h1>
+</div> 
+
+<br><br>
+<?php 
+if ( $xlsx = SimpleXLSX::parse('data/former_graduates.xlsx') ) {
+  $i = 0;
+    foreach ($xlsx->rows() as $elt){
+      if ($i == 0) {
+        echo '<div class="row">';
+      } else {
+        echo '<div class="column">
+        <div class="card">
+        <img class="img-pro" src="'.$elt[7].'" alt="Jane">
+          <div class="container">
+              <h2>'.$elt[1].'</h2>
+              <p class="title">'.$elt[2].'</p>
+              <p class="title">'.$elt[3].'</p>
+            </div>
+          </div>
+        </div>';
+      }   
+  $i = $i + 1;   
+  }
+  echo '</div>';
+} else {
+  echo SimpleXLSX::parseError();
+}
 ?>
 
 </body>
